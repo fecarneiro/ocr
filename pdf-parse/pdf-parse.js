2669a6741e2571aa73632d9522bbcb7992c1e6b2
Felipe <fe.iacarneiro@gmail.com>
2025-10-28 17:00:21 -0300

    add new feature

--- pdf-parse/pdf-parse.js
+++ pdf-parse/pdf-parse.js
@@ -2,12 +2,13 @@ import { PDFParse } from 'pdf-parse';
 import fs from 'node:fs/promises';
 
 async function main(file) {
-  const readFile = await fs.readFile(file);
-  const parser = new PDFParse(file);
-
-  const result = await parser.getText();
+  console.time('timer');
+  const buffer = await fs.readFile(file);
+  const parser = new PDFParse({ data: buffer });
+  const result = await parser.getText(parser);
   console.log(result.text);
+  console.timeEnd('timer');
 }
 
 const file = 'dta.pdf';
-run(file);
+main(file);
