d6deb3a17f13ed584edc253cd6ef966fda65bfca
Felipe <fe.iacarneiro@gmail.com>
2025-10-28 08:28:05 -0300

    add new feature

--- index.js
+++ index.js
@@ -6,14 +6,16 @@ async function main(pdfPath) {
   const document = await pdf(pdfPath, { scale: 3 });
   const worker = await createWorker('por');
 
+  let file = Math.random() * 100;
   for await (const image of document) {
     const {
       data: { text },
     } = await worker.recognize(image);
-    await fs.writeFile('extraction.txt', text, 'utf8');
+    await fs.appendFile(`extraction${file}.txt`, text, 'utf8');
     console.log(text);
-    await worker.terminate();
   }
+  await worker.terminate();
 }
 
-main('example.pdf');
+const pdfPath = process.argv[2] || 'di.pdf';
+main(pdfPath);
