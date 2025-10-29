d2dfef4adda703e4663c961d1510c99212313f9a
Felipe <fe.iacarneiro@gmail.com>
2025-10-29 12:18:22 -0300

    start: refactoring project structure

--- regex/dtaRegex.js
+++ regex/dtaRegex.js
@@ -8,3 +8,19 @@ const dtaGeralRegex = {
 const dtaLocationRegex = /Unidade\s+Local\s*:\s*([^\n]+)/g;
 
 export { dtaGeralRegex, dtaLocationRegex };
+
+let result = {};
+for await (const [key, value] of Object.entries(dtaGeralRegex)) {
+  const fullMatch = extractedText.match(value);
+  const match = fullMatch ? fullMatch[1] : null;
+  result[key] = match;
+}
+
+const locations = [];
+const location = extractedText.matchAll(dtaLocationRegex);
+for (const x of location) {
+  locations.push(x[1]);
+}
+
+result.origin = locations[0];
+result.destination = locations[1];
