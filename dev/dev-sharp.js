4b90ec183507ccd3bcf931e9509a3aefd339c943
Felipe <fe.iacarneiro@gmail.com>
2025-10-30 15:09:16 -0300

    add new feature

--- dev/dev-sharp.js
+++ dev/dev-sharp.js
@@ -1,1 +1,17 @@
 import sharp from 'sharp';
+import fs from 'node:fs/promises';
+
+async function main(input) {
+  const metadata = await sharp(input).metadata();
+  console.log(metadata);
+
+  const greyscale = await sharp(input)
+    .greyscale()
+    .withMetadata({ density: 300 })
+    .toFile('/dev/dev.png');
+  const greyscaleMeta = await sharp(greyscale).metadata();
+  console.log(greyscaleMeta);
+}
+
+const image = 'pic/png1.png';
+main(image);
