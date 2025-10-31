85e43765c0ca7310310edb4e5eba6f17a8ec27a5
Felipe <fe.iacarneiro@gmail.com>
2025-10-31 10:39:38 -0300

    add new feature

---
+++ eslint.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig(
+  eslint.configs.recommended,
+  tseslint.configs.recommended
+);
+
+// testar outras configs depois
+// https://typescript-eslint.io/getting-started
