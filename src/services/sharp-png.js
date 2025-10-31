7f6fb904b53ce9fb7c536c54b2e1a7c6a6eacbfd
Felipe <fe.iacarneiro@gmail.com>
2025-10-31 09:07:17 -0300

    add new feature

--- src/services/sharp-png.js
+++ src/services/sharp-png.js
@@ -1,12 +1,14 @@
 import sharp from 'sharp';
 
 async function sharpPNG(pngFile) {
+  console.log('antes:', await sharp(pngFile).metadata());
   const ocrReadyImage = await sharp(pngFile)
     .greyscale()
     .sharpen()
     .withMetadata({ density: 300 })
     .toBuffer();
 
+  console.log('depois:', await sharp(ocrReadyImage).metadata());
   return ocrReadyImage;
 }
 
