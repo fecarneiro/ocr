9771b93d8e80cf7c7ae8e406f0d3347b93743f3b
Felipe <fe.iacarneiro@gmail.com>
2025-11-04 15:22:45 -0300

    refactor: streamline OCR extraction process and enhance error handling

--- src/core/ocr-processor.ts
+++ src/core/ocr-processor.ts
@@ -3,6 +3,7 @@ import { createWorker } from 'tesseract.js';
 import fs from 'node:fs/promises';
 import { matchFields } from '../services/dta-service.js';
 import { sharpPNG } from '../services/image-service.js';
+import sharp from 'sharp';
 
 async function createTesseractWorker() {
   const worker = await createWorker('por', 1, {
@@ -12,24 +13,45 @@ async function createTesseractWorker() {
   return worker;
 }
 
-async function pdfToImage(pdfFile: string | Buffer) {
+async function pdfToImage(pdfFile: string | Buffer): Promise<ArrayBuffer[]> {
   let counter = 1;
   const pages = [];
   const document = await pdf(pdfFile, { scale: 2 });
   for await (const image of document) {
     await fs.writeFile(`./data/output/page${counter}.png`, image);
+    pages.push(image);
     counter++;
   }
+  return pages;
 }
+async function optimizeImage(pages: ArrayBuffer[]) {
+  console.log('buffer length ', pages.length);
+  let counter = 1;
+  const optimizedPages = [];
+  for await (const page of pages) {
+    console.log('input page', page);
+    const optmizedPage = await sharp(page)
+      .greyscale()
+      .normalise()
+      .sharpen()
+      .toBuffer();
+    await fs.writeFile(`./data/output/opt-page${counter}.png`, optmizedPage);
+    optimizedPages.push(optmizedPage);
+    console.log('optmized img', optmizedPage);
+    counter++;
+  }
+  return optimizedPages;
+}
+
 async function tryOCRExtraction(pdfFile: string | Buffer) {
   console.log('iniciando OCR para: ', pdfFile);
   try {
     const worker = await createTesseractWorker();
     // const images = pdfToImage(pdfFile);
+    // const optimizedImages = optmizeImage(pdfFile);
     let data: string = '';
 
     for await (const image of document) {
-      const ocrReadyImage = await sharpPNG(image);
       const {
         data: { text },
       } = await worker.recognize(ocrReadyImage);
@@ -55,4 +77,4 @@ async function tryOCRExtraction(pdfFile: string | Buffer) {
   }
 }
 
-export { tryOCRExtraction, pdfToImage };
+export { tryOCRExtraction, pdfToImage, optimizeImage };
