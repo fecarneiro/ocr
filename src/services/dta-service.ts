a7fe3ea09cf1dbdbfa798c39a29cef8e1939f6aa
Felipe <fe.iacarneiro@gmail.com>
2025-11-05 17:24:49 -0300

    chore: remove unused code and improve regex matching logic

--- src/services/dta-service.ts
+++ src/services/dta-service.ts
@@ -1,5 +1,10 @@
 import type { DtaResult } from '../types/index.js';
-const dtaRegex = {
+
+type DtaRegexConfig = {
+  [key in keyof DtaResult]: RegExp;
+};
+
+const dtaRegex: DtaRegexConfig = {
   cnpjEmbarcador: /CNPJ(?:\/CPF)?\s+do\s+Beneficiário\s*:\s*([^\n]+)/i,
   nomeBeneficiario: /Nome\s+do\s+Beneficiário\s*:\s*([^\n]+)/i,
   valorCarga: /Valor\s+da\s+Carga\s+em\s+Moeda\s+Nacional\s*:\s*([^\n]+)/i,
@@ -10,22 +15,15 @@ const dtaRegex = {
     /(?<=destino|dest\s*ino)[^\n]*\n[\s\S]*?Unidade\s+Local\s*:\s*(\d+\s*-[^\n]+)/i,
 };
 
-function matchFieldsWithRegex(data: string) {
-  const regexExtraction: DtaResult = {
-    cnpjEmbarcador: null, // ou ''
-    nomeBeneficiario: null,
-    valorCarga: null,
-    descricaoCarga: null,
-    origem: null,
-    destino: null,
+function matchFieldsWithRegex(data: string): DtaResult {
+  return {
+    cnpjEmbarcador: data.match(dtaRegex.cnpjEmbarcador)?.[1] ?? null,
+    nomeBeneficiario: data.match(dtaRegex.nomeBeneficiario)?.[1] ?? null,
+    valorCarga: data.match(dtaRegex.valorCarga)?.[1] ?? null,
+    descricaoCarga: data.match(dtaRegex.descricaoCarga)?.[1] ?? null,
+    origem: data.match(dtaRegex.origem)?.[1] ?? null,
+    destino: data.match(dtaRegex.destino)?.[1] ?? null,
   };
-  //opt chaining + nullish coalescing operator javascript
-  for (const [key, value] of Object.entries(dtaRegex)) {
-    const fullMatch = data.match(value);
-    const match = fullMatch?.[1] ?? null;
-    regexExtraction[key as keyof DtaResult] = match;
-  }
-
-  return regexExtraction;
 }
+
 export { matchFieldsWithRegex };
