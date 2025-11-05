80cc6e9a15c071175750bc0c3fb09a95a9eaf638
Felipe <fe.iacarneiro@gmail.com>
2025-11-05 07:13:36 -0300

    refactor: remove unused PDF extraction and image processing code

--- dev/images.ts
+++
@@ -1,7 +0,0 @@
-import { pdfToImage, optimizeImage } from '../src/core/ocr-processor';
-
-async function imageExtraction(file) {
-  const images = await pdfToImage(file);
-  await optimizeImage(images);
-}
-imageExtraction('./data/input/dta5.pdf');
