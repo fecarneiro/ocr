eda7b2906c7f6faa1cbefb9fad1ff4b03b0bc6a5
Felipe <fe.iacarneiro@gmail.com>
2025-11-05 19:16:33 -0300

    chore: add initial server implementation with upload endpoint

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import fastify from 'fastify';
+
+const server = fastify({
+  // logger: true,
+});
+
+server.post('/upload', async (request, reply) => {
+  console.log(request.body);
+  return 'pong\n';
+});
+
+server.listen({ port: 8080 }, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Server listening at ${address}`);
+});
