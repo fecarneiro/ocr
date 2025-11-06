bddd9d575d3ad1275a3edf92cdada883d50a1988
Felipe <fe.iacarneiro@gmail.com>
2025-11-06 17:40:54 -0300

    refactor: enhance PDF parsing function for better performance and readability

--- .dev/text-parse.ts
+++ .dev/text-parse.ts
@@ -2,11 +2,11 @@ import { PDFParse } from 'pdf-parse';
 import fs from 'node:fs/promises';
 
 async function fn(file: string) {
-  const read = await fs.readFile(file, 'utf8');
-  const parser = new PDFParse({ data: read });
+  const buffer = await fs.readFile(file);
+  const parser = new PDFParse({ data: buffer });
   const result = await parser.getText();
   await parser.destroy();
   console.log(result.text);
 }
 
-await fn('./data/input/dta1.pdf');
+await fn('./data/input/nfe1.pdf');
