8c60040d4a4e12a9b689e6d91c928a2f8f9653df
Felipe <fe.iacarneiro@gmail.com>
2025-11-06 07:57:10 -0300

    chore: remove routes file as it is no longer needed

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import express, { type Request, type Response } from 'express';
-import dotenv from 'dotenv';
 
+const PORT = 3000;
 const app = express();
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World');
 });
 
-app.listen(3000);
+app.listen(PORT);
