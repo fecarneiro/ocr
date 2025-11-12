aba6917942508ac22bed0e67b425d5e6768fd525
Felipe <fe.iacarneiro@gmail.com>
2025-11-12 11:42:50 -0300

    feat: add initial route for DTA processing

---
+++ src/server/server.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import config from './config/env.js';
+
+const app = express();
+app.use(express.json());
+
+app.listen(config.port, () => {
+  console.log(`Server listening on port {config.port}`);
+});
