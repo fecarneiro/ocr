dda8ed53762cc0e5f532498bad60f35adcce7870
Felipe <fe.iacarneiro@gmail.com>
2025-11-14 07:49:57 -0300

    feat: add initial home page for document upload functionality

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import path from 'node:path';
+import { fileURLToPath } from 'url';
 import { config } from './config/env.js';
 import documentsRouter from './routes/routes.js';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
+
+app.use('/', (req, res) => {
+  res.render(path.join(__dirname, 'public', 'home.html'));
+});
 
 app.use('/api/upload', documentsRouter);
 
