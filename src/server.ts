40f8574d9722ae42419caf04252d05e5773c27cb
Felipe <fe.iacarneiro@gmail.com>
2025-11-14 16:38:12 -0300

    fix: update server configuration for improved performance

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,8 @@
-import dotenv from 'dotenv';
 import express from 'express';
 import path from 'node:path';
 import { fileURLToPath } from 'url';
 import { config } from './config/env.js';
 import documentsRouter from './routes/upload-routes.js';
-dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -17,5 +15,4 @@ app.use('/api/upload', documentsRouter);
 
 app.listen(config.port, () => {
   console.log(`Server listening on port ${config.port}`);
-  console.log(config);
 });
