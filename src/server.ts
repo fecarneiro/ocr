407d93be8958b29d09c6c88af5e81c9f3775224a
Felipe <fe.iacarneiro@gmail.com>
2025-11-14 15:51:53 -0300

    fix: update environment configuration handling

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
+import dotenv from 'dotenv';
 import express from 'express';
 import path from 'node:path';
 import { fileURLToPath } from 'url';
 import { config } from './config/env.js';
 import documentsRouter from './routes/upload-routes.js';
+dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -15,4 +17,5 @@ app.use('/api/upload', documentsRouter);
 
 app.listen(config.port, () => {
   console.log(`Server listening on port ${config.port}`);
+  console.log(config);
 });
